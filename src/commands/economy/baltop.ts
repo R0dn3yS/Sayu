ef3d9e20a1f3d85506212ef3d8d6b1335b5d00b8
Rodney van den Velden <rodneymichael2002@gmail.com>
2025-04-15 01:10:23 +0200

    fmt

--- src/commands/economy/baltop.ts
+++ src/commands/economy/baltop.ts
@@ -1,4 +1,4 @@
-import { EmbedBuilder } from "discord.js";
+import { EmbedBuilder } from 'discord.js';
 import { stripIndents } from 'common-tags';
 import { Command, CommandContext } from '../../../deps.ts';
 import { db } from '../../index.ts';
