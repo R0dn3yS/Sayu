aeb04e5cfc39c7b735fc2f58c2fe8135218fe3a8
Rodney van den Velden <rodneymichael2002@gmail.com>
2025-12-29 20:44:53 +0100

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from 'https://raw.githubusercontent.com/R0dn3yS/djs-commandclient/6fb52f3f4e228e6fcc41f56e785f80bdff2548b6/index.ts';
-export * from 'https://raw.githubusercontent.com/R0dn3yS/discord-music/81a54e78bf09088de3d1452da72334937cb73cc6/index.ts';
+export * from 'https://raw.githubusercontent.com/R0dn3yS/djs-commandclient/58cfcf2a9c7990109951f48f702f51e616be8c67/index.ts';
+export * from 'https://raw.githubusercontent.com/R0dn3yS/discord-music/020b23bb0016a681eeb61bdf4bc8dfb2daa284d8/index.ts';
